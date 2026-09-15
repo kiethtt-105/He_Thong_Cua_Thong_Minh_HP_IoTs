d3a336feff71816c3615b9791ffbe7baf6d7b971
Huỳnh Trần Tuấn Kiệt <kiethtt.105204@gmail.com>
2026-09-15 15:44:00 +0700

    DEL

--- smart_lock/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    # Trang chính của ứng dụng accounts (login, register, dashboard...)
-    path('', include('accounts.urls')),
-    # Admin site
-    path('admin/', admin.site.urls),
-]
