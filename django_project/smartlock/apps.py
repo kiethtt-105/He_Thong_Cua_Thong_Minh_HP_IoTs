e69fc5fa0fa38792fdbece0c5f5509ddf5d67fc4
Huỳnh Trần Tuấn Kiệt <kiethtt.105204@gmail.com>
2026-09-18 16:25:46 +0700

    del

--- django_project/smartlock/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmartlockConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'smartlock'
-    verbose_name = 'SmartLock'
