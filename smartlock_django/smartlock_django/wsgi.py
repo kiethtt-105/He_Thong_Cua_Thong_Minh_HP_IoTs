b3d3c7251672fd6d11da991a2ee0bab6f4388779
Huỳnh Trần Tuấn Kiệt <kiethtt.105204@gmail.com>
2026-09-20 10:42:58 +0700

    Add staticfiles app with WSGI and ASGI support
    Add staticfiles app with WSGI and ASGI support
    
    - Implemented StaticFilesHandler and ASGIStaticFilesHandler to serve static files in both WSGI and ASGI applications.
    - Created management commands for collecting static files, finding static files, and running the development server with static file serving.
    - Introduced StaticFilesStorage for managing static file storage with support for hashed filenames and manifest files.
    - Added utility functions for matching patterns and checking settings related to static files.
    - Developed views for serving static files during development.
    - Enhanced testing capabilities with StaticLiveServerTestCase to facilitate testing of static file serving without needing to run collectstatic.
    - Updated URL patterns to include static file serving in development mode.Add staticfiles app with WSGI and ASGI support
    
    - Implemented StaticFilesHandler and ASGIStaticFilesHandler to serve static files in both WSGI and ASGI applications.
    - Created management commands for collecting static files, finding static files, and running the development server with static file serving.
    - Introduced StaticFilesStorage for managing static file storage with support for hashed filenames and manifest files.
    - Added utility functions for matching patterns and checking settings related to static files.
    - Developed views for serving static files during development.
    - Enhanced testing capabilities with StaticLiveServerTestCase to facilitate testing of static file serving without needing to run collectstatic.
    - Updated URL patterns to include static file serving in development mode.

--- smartlock_django/smartlock_django/wsgi.py
+++ smartlock_django/smartlock_django/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
-
+import os,sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartlock_django.settings')
 
 application = get_wsgi_application()
+app = application
